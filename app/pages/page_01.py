c4894dbcc1b910d833882aa0e5a5c8ead0590850
Nils Coupey <coupey.nils@gmail.com>
2025-06-21 07:52:56 +0200

    adding local password

--- app/pages/page_01.py
+++ app/pages/page_01.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 secret_password = st.secrets["password"]
+local_secret_password = st.secrets["local_password"]
 
 st.write('My password is : ' + secret_password)
+
+st.write('My local password is : ' + local_secret_password)
