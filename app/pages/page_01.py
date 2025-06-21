538a06c7ad672302c18354e9b4947954b7dcf7dd
Nils Coupey <coupey.nils@gmail.com>
2025-06-21 07:54:45 +0200

    correction

--- app/pages/page_01.py
+++ app/pages/page_01.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-secret_password = st.secrets["password"]
-local_secret_password = st.secrets["local_password"]
+#secret_password = st.secrets["password"]
 
-st.write('My password is : ' + secret_password)
+#st.write('My password is : ' + secret_password)
+
+local_secret_password = st.secrets["local_password"]
 
 st.write('My local password is : ' + local_secret_password)
